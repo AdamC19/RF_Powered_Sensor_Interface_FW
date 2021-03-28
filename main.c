15ef2adce9259698e94f0707a0edb36142cea287
AdamC19 <cordingleyadam19+github@gmail.com>
2021-03-28 11:53:24 -0400

    Add various clock and power management functions

--- main.c
+++ main.c
@@ -45,11 +45,19 @@
 
 /* User Includes */
 
+/* Macros */
+#define DISABLE_I2C   (PMD4 |= _PMD4_MSSP1MD_MASK)
+#define ENABLE_I2C    (PMD4 &= ~_PMD4_MSSP1MD_MASK)
+#define DISABLE_UART  (PMD4 |= _PMD4_UART1MD_MASK)
+#define ENABLE_UART   (PMD4 &= ~_PMD4_UART1MD_MASK)
+
 /* Global Variables */
 
 
 /* Function Prototypes */
-void stuff();
+void enter_listen_mode();
+void enter_normal_mode();
+void enter_hi_perf_mode();
 
 /*
                          Main application
@@ -63,10 +71,10 @@ void main(void)
     // Use the following macros to:
 
     // Enable the Global Interrupts
-    //INTERRUPT_GlobalInterruptEnable();
+    INTERRUPT_GlobalInterruptEnable();
 
     // Enable the Peripheral Interrupts
-    //INTERRUPT_PeripheralInterruptEnable();
+    INTERRUPT_PeripheralInterruptEnable();
 
     // Disable the Global Interrupts
     //INTERRUPT_GlobalInterruptDisable();
@@ -79,6 +87,75 @@ void main(void)
         // Add your application code
     }
 }
+
+/**
+ * @brief enter the mode to listen for received frames\
+ * 
+ * The CPU is put into Doze mode. The peripherals continue to operate at the system clock
+ * frequency. Recover on interrupt is set so the ISR is executed at the system clock
+ * speed. Doze on exit is set so we return to Doze mode after an ISR.
+ */
+void enter_listen_mode(){
+  // disable I2C clock
+  DISABLE_I2C;
+  DISABLE_UART;
+
+  // set OSCFRQ to use the internal oscillator at 16MHz
+  OSCFRQ &= 0xF0; // clear
+  OSCFRQ |= 0x06; // set nominal freq to 16MHz
+
+  // set NOSC<2:0> to request HFINTOSC at 16MHz
+  OSCCON1 &= ~(_OSCCON1_NOSC_MASK); // reset bits
+  OSCCON1 |= (0x6 << _OSCCON1_NOSC_POSITION) & _OSCCON1_NOSC_MASK; // set accordingly
+
+  // enable ROI (Recover On Interrupt) so interrupt executes at full speed
+  CPUDOZE |= _CPUDOZE_ROI_MASK;
+
+  // enable DOE (Doze On Exit) so we go back to doze mode after interrupt handling
+  CPUDOZE |= _CPUDOZE_DOE_MASK;
+  
+  // now set DOZE<2:0> to desired ratio
+  CPUDOZE |= (3 << _CPUDOZE_DOZE_POSITION) & _CPUDOZE_DOZE_MASK; // 3 gives a ratio of 1:16
+  // finally enter DOZE mode (set DOZEN bit)
+  CPUDOZE |= _CPUDOZE_DOZEN_MASK;
+}
+
+
+void enter_normal_mode(){
+  // exit DOZE mode (reset DOZEN bit)
+  CPUDOZE &= ~_CPUDOZE_DOZEN_MASK;
+
+  // set OSCFRQ to use the internal oscillator at 2MHz
+  OSCFRQ &= 0xF0; // clear
+  OSCFRQ |= 0x01; // set nominal freq to 2MHz
+
+  // set NOSC<2:0> to 6 (110) to use HFINTOSC at 1MHz
+  OSCCON1 &= ~_OSCCON1_NOSC_MASK; // first ensure all bits are reset
+  OSCCON1 |= (0x6 << _OSCCON1_NOSC_POSITION) & _OSCCON1_NOSC_MASK; // now set
+
+  ENABLE_I2C;
+  ENABLE_UART; 
+  
+}
+
+
+void enter_hi_perf_mode(){
+  // exit DOZE mode (reset DOZEN bit)
+  CPUDOZE &= ~_CPUDOZE_DOZEN_MASK;
+
+  // set OSCFRQ to use the internal oscillator at 16MHz
+  OSCFRQ &= 0xF0; // clear
+  OSCFRQ |= 0x06; // set nominal freq to 16MHz
+
+  // set NOSC<2:0> to request HFINTOSC at 16MHz
+  OSCCON1 &= ~(_OSCCON1_NOSC_MASK); // reset bits
+  OSCCON1 |= (0x6 << _OSCCON1_NOSC_POSITION) & _OSCCON1_NOSC_MASK; // set accordingly
+
+  ENABLE_I2C;
+  ENABLE_UART;
+
+}
+
 /**
  End of File
 */
